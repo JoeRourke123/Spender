5aa19558d6ff326ead9f273236c9650425b7e030
Joe Rourke <hello@joerourke.co.uk>
2019-11-28 18:06:04 +0000

    Created button stage for new Transaction

--- src/checkpoint/Main.java
+++ src/checkpoint/Main.java
@@ -1,22 +1,63 @@
 package checkpoint;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-import java.util.Date;
-
 public class Main extends Application {
+    public void createPopup(Transaction t) {
+        createPopup(t.getTitle(), t.getAmount(), t.getCategory(), t.getTime());
+    }
+
+    public void createPopup() {
+        createPopup("", 0.0, "", "");
+    }
+
+    public void createPopup(String title, double amount, String category, String date) {
+        Stage newTransactionStage = new Stage();
+                VBox newTransactionBox = new VBox();
+                newTransactionBox.setSpacing(10);
+                newTransactionBox.setAlignment(Pos.CENTER);
+
+
+                TextField titleField = new TextField(), amountField = new TextField(), categoryField = new TextField();
+                titleField.setMaxWidth(150); amountField.setMaxWidth(150); categoryField.setMaxWidth(150);
+                titleField.setPromptText("Title"); amountField.setPromptText("£"); categoryField.setPromptText("Category");
+
+                TextField day = new TextField(), month = new TextField(), year = new TextField();
+                day.setMaxWidth(40); month.setMaxWidth(40); year.setMaxWidth(55);
+                day.setPromptText("DD"); month.setPromptText("MM"); year.setPromptText("YYYY");
+                HBox dateField = new HBox();
+                dateField.getChildren().addAll(day, month, year);
+                dateField.setSpacing(5);
+                dateField.setMaxWidth(150);
+
+                if(!title.isEmpty() && amount != 0 && !category.isEmpty() && !date.isEmpty()) {
+                    titleField.setText(title);
+                    amountField.setText(String.valueOf(amount));
+                    categoryField.setText(category);
+                    String[] splitDate = date.split("-");
+                    day.setText(splitDate[0]); month.setText(splitDate[1]); year.setText(splitDate[2]);
+                }
+
+                Button save = new Button("Save");
+
+                newTransactionBox.getChildren().addAll(
+                        titleField, amountField, categoryField,
+                        dateField,
+                        save
+                );
+
+                newTransactionStage.setScene(new Scene(newTransactionBox, 200, 200));
+                newTransactionStage.show();
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -64,6 +105,11 @@ public class Main extends Application {
         totalIns.setText(totalIns.getText() + "24.54");
 
         Button newTransactionBtn = new Button("+ New Transaction");
+        newTransactionBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                createPopup();
+            }
+        });
         vbox.getChildren().addAll(totalIns, totalOuts, cashFlow, newTransactionBtn);
 
         stage.setTitle("Budgeting");
